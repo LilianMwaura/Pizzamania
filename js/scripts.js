f298e4224fe6837f97a370d280392a0e5ce989aa
LilianMwaura <wambuimwaura28@gmail.com>
2022-03-21 09:35:51 +0300

    add else if functions

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,74 @@ $(document).ready(function () {
         }
         let topping_value = ptopping.length * 50;
         console.log("toppins value" + topping_value);
+        if ((psize == "0") && (pcrust == "0")) {
+            console.log("nothing selected");
+            $("button.proceed").show();
+            $("#information").show();
+            $("div.choise").hide();
+            alert("Please select pizza size and crust");
+        }
+        else {
+            $("button.proceed").hide();
+            $("#information").hide();
+            $("div.choise").slideDown(1000);
+        }
+
+        total = price + crust_price + topping_value;
+        console.log(total);
+        let checkoutTotal = 0;
+        checkoutTotal = checkoutTotal + total;
+
+        $("#pizzaname").html($(".name option:selected").val());
+        $("#pizzasize").html($("#size option:selected").val());
+        $("#pizzacrust").html($("#crust option:selected").val());
+        $("#pizzatopping").html(ptopping.join(", "));
+        $("#totals").html(total);
+        // Addition of pizza button
+        $("button.addPizza").click(function () {
+            let pname = $(".name option:selected").val();
+            let psize = $("#size option:selected").val();
+            let pcrust = $("#crust option:selected").val();
+            let ptopping = [];
+            $.each($("input[name='toppings']:checked"), function () {
+                ptopping.push($(this).val());
+            });
+            console.log(ptopping.join(", "));
+            if (psize == "large") {
+                price = 1200;
+                console.log("The price is Ksh." + price);
+            }
+            else if (psize == "medium") {
+                price = 850;
+                console.log("The price is Ksh." + price);
+            }
+            else if (psize == "small") {
+                price = 600;
+                console.log("The price is Ksh." + price);
+            }
+            else {
+                price = 0;
+            }
+            switch (pcrust) {
+                case "0":
+                    crust_price = 0;
+                    break;
+                case "Crispy":
+                    crust_price = 200;
+                    break;
+                case "Stuffed":
+                    crust_price = 150;
+                    break;
+                case "Gluten-free":
+                    crust_price = 180;
+                    break;
+                default:
+                    console.log("No price");
+            }
+            let topping_value = ptopping.length * 50;
+            console.log("toppins value" + topping_value);
+            total = price + crust_price + topping_value;
+            console.log(total);
+
+            checkoutTotal = checkoutTotal + total;
+            console.log(checkoutTotal);
