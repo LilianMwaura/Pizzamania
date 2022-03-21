23b64fde826c09123cae83bafbd5d81215e15156
LilianMwaura <wambuimwaura28@gmail.com>
2022-03-21 09:37:04 +0300

    add constractor function

--- js/scripts.js
+++ js/scripts.js
@@ -123,3 +123,10 @@ $(document).ready(function () {
 
             checkoutTotal = checkoutTotal + total;
             console.log(checkoutTotal);
+             // constractor function
+             var newOrder = new Getpizza(pname, psize, pcrust, ptopping, total);
+
+             $("#ordersmade").append('<tr><td id="pizzaname">' + newOrder.name + '</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">' + newOrder.crust + '</td><td id="pizzatopping">' + newOrder.topping + '</td><td id="totals">' + newOrder.total + '</td></tr>');
+             console.log(newOrder);
+         });
+         
